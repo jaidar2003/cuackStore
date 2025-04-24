58931ff89012482d96dbc70a0d0894be8137cf03
meschinidino <francodmsoloing@gmail.com>
2025-04-24 20:14:23 -0300

    first commit

---
+++ src/main/java/com/example/cuakstore/CuakStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.cuakstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuakStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuakStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cuakstore/CuakStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cuakstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuakStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
